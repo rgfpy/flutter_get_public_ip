42154a49e0be56a2f932f2d565cb6d2a8f0870e6
Robert Galeano <r2galeano@gmail.com>
2024-03-14 15:54:21 +0000

    allow public access to getMyIP

--- lib/src/wifi_details.dart
+++ lib/src/wifi_details.dart
@@ -2,6 +2,9 @@ import 'package:wifi_ip_details/src/models/ip_details.dart';
 import 'package:wifi_ip_details/src/services/ip_service.dart';
 
 class WifiIPDetails {
+  static Future<String?> getMyIP() async {
+    return await IPService().getMyIP();
+  }
   static Future<IPDetails?> getMyWIFIDetails([String? accessToken]) async {
     return await IPService().getWIFIDetails(accessToken);
   }
